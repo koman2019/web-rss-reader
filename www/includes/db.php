c3674c225c1ddcb64db59150a1314be894348b1c
koman2019 <koman2019@gmail.com>
2017-04-06 17:28:31 +0800

    test db and add background image if no item in your own source

--- www/includes/db.php
+++ www/includes/db.php
@@ -1,7 +1,7 @@
 <?php 
-$DB_HOST = '127.0.0.1';
-$DB_USER = 'root';
-$DB_PASS = '';
+$DB_HOST = '127.2.208.130';
+$DB_USER = 'adminADmfXgs';
+$DB_PASS = 'iPa1PjAm9U52';
 $DB_NAME = 'reader';
 $mysqli = new mysqli($DB_HOST, $DB_USER, $DB_PASS, $DB_NAME);
 ?>
