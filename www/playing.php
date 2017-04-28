1dac45c1edcb3dc091aff3582c36b4a52bc6aade
koman2019 <koman2019@gmail.com>
2017-04-28 16:19:12 +0800

    fix typo and rename the project

--- www/playing.php
+++ www/playing.php
@@ -15,14 +15,14 @@
 		<link rel="stylesheet" type="text/css" href="css/taskman.css"/>
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=News+Cycle:400,700">
 		<link rel="stylesheet" href="fonts/font-awesome.min.css">
-		<title>Read Around </title>
+		<title>News Carpet </title>
 
     </head>
     <body>
 	
 		<nav class="navbar navbar-default">
 			<div class="container">
-				<div class="navbar-header"><a class="navbar-brand navbar-link" href="index.html">Read-Around</a></div>
+				<div class="navbar-header"><a class="navbar-brand navbar-link" href="index.html">News Carpet</a></div>
 				<div class="collapse navbar-collapse" id="navcol-1">
 					<ul class="nav navbar-nav">
 						<li role="presentation"><a href="main.php">Main board</a></li>
@@ -100,7 +100,7 @@
 				<hr>
 				<div class="row">
 					<div class="col-sm-6">
-						<h5>Read Around © 2017</h5>
+						<h5>News Carpet © 2017</h5>
 					</div>
 				</div>
 			</div>
